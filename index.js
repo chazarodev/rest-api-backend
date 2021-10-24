61107d7445300b1681041535304addb510e53264
Alberto Cházaro <shazarerom@gmail.com>
2021-10-23 20:41:36 -0500

    Habilitando cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
+//Importar cors para permitir el consumo de la api en un tercero
+const cors = require('cors');
+
 //Conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/restapis', {
@@ -14,6 +17,9 @@ const app = express();
 //Habilitar bodyparser
 app.use(express.json()); //BodyParser is deprecated, this works as well
 app.use(express.urlencoded({extended: true}));
+
+//Habilitar cors
+app.use(cors());
 
 //Rutas de la app
 app.use('/', routes());
